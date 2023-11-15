a758c86092d844cefe7e4f0874b4d27707078482
Marcus Girolneto <marcus.girolneto@gmail.com>
2023-11-15 15:59:53 -0300

    Adding moment

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import moment from 'moment'
 import { SendCommandCommand, GetParameterCommand } from '@aws-sdk/client-ssm'
 import { LAYERS, NETWORK_NODES } from '../utils/types.js'
 
